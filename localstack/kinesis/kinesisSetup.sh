a876437c08df5ff3d80a9f9e175ef405300919d2
Nick White <nicholas.white752@gmail.com>
2024-03-06 21:05:52 -0500

    kinesis localstack setup

---
+++ localstack/kinesis/kinesisSetup.sh
@@ -0,0 +1,10 @@
+echo "Starting Script"
+
+# Logs into docker container
+# docker exec -it localhost-localstack-main sh
+
+# Create Kinesis Stream
+awslocal kinesis create-stream \
+  --stream-name lambda-stream-input \
+  --shard-count 1
+
